8c28b607b52b0076a69b83711d72e179490e9f16
vijaykumar208 <vijaydornadula@gmail.com>
2024-06-26 19:24:52 +0530

    second commit

--- src/main/java/com/example/CrudOperations/controller/CustomerController.java
+++ src/main/java/com/example/CrudOperations/controller/CustomerController.java
@@ -3,9 +3,7 @@ package com.example.CrudOperations.controller;
 import com.example.CrudOperations.entity.Customer;
 import com.example.CrudOperations.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,23 +12,24 @@ public class CustomerController {
       @Autowired
       public CustomerService customerservice;
 
-
+      @GetMapping("/customers")
       private List<Customer> getAllCustomers(){
 
           return customerservice.getAllCustomers();
       }
-
+      @GetMapping("/{getbyid}")
       private Customer getCustomer(@PathVariable("id") int id){
 
           return customerservice.getById(id);
       }
+      @PostMapping("/customer")
       public Customer saveCustomer(@RequestBody Customer customer){
 
            customerservice.save(customer);
 
            return customer;
       }
-
+      @PutMapping("/customers")
       private Customer UpdateCustomer(@RequestBody Customer customer){
 
           customerservice.save(customer);
--- src/main/java/com/example/CrudOperations/entity/Customer.java
+++ src/main/java/com/example/CrudOperations/entity/Customer.java
@@ -3,8 +3,14 @@ package com.example.CrudOperations.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Data
+@Getter
+@Setter
 public class Customer {
     @Id
     @GeneratedValue
@@ -16,35 +22,4 @@ public class Customer {
 
     private String address;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
